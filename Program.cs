209f55e73c53eb5019c2c9c17aaa3b5f2b82475a
Dell-User <Dell-User@DESKTOP-BSDRSH6>
2023-10-10 11:05:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number ");
+            int num1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another number ");
+            int num2= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 / num2);
+
+          Exception e;
+
+            Console.WriteLine(e.Message);
+        }
+        //freeze console.Readline();
+    }
+}
